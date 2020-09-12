47c96eeb191afce01b6877567ccbb333e674eb06
pradeepamatya <pradeepbdramatya@gmail.com>
2020-09-12 07:47:49 +0545

    adding welcome page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core'
+import { AuthenticationService } from '../authentication.service';
 
 @Component({
     templateUrl: './home.component.html'
 })
 
-export class HomeComponent{}
+export class HomeComponent{
+    constructor(public auth: AuthenticationService){ }
+}
